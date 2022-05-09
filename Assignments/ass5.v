6abec17eb68d343e31b5e56026e36a138e02f9c7
Faith Olopade <91458920+olopadef@users.noreply.github.com>
2022-05-09 12:05:41 +0100

    Add files via upload

---
+++ Assignments/ass5.v
@@ -0,0 +1,12 @@
+module AdderSub (sum_diff, carry, A, B, select);
+     			output reg[3:0] sum_diff;
+     			output reg carry;
+     			input select;
+     			input[3:0] A, B;
+     		
+     			always @(select, A, B)
+     				case(select)
+     					1'b0: assign{carry, sum_diff} = A+B;
+    					1'b1: assign{carry, sum_diff} = A+{~B}+select;
+    				endcase
+endmodule
